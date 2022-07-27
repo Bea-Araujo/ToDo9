418d10bd741371221882986163ba673ac2740ccc
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-07-27 19:15:07 -0300

    responsividade ShareSection adicionada

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import S from './Input.module.css'
 
-export const Input = ({label, type}) => {
+export const Input = ({ label, type }) => {
   return (
-    <fieldset className={S.fieldset}>
-        <label className={S.label}>{label}</label>
-        <input className={S.input} type={type}/>
+    <fieldset className={S.fieldset} width='100%'>
+      <label className={S.label}>{label}</label>
+      <input className={S.input} type={type} />
     </fieldset>
   )
 }
