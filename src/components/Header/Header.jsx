b89edff2c02218637ebd93630b82b8376731bc63
Bea-Araujo <beatriz.a.0702@gmail.com>
2022-07-27 18:36:14 -0300

    responsividade do header adicionada

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import S from './Header.module.css'
 
 export const Header = () => {
-  return (
-    <header className={S.header}>
-        {/* gadi was here */}
+	return (
+		<header className={S.header}>
+			{/* gadi was here */}
 
-        <div className={S.container}>
-			
-			<article>
-			<p className={S.p1}>uma seleção especial de produtos</p>
-			<h3 className={S.boldText}>especial para você</h3>
-			</article>
+			<div className={S.container}>
 
-			<p className={S.p2}>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
-			
-			<div className={S.btndiv}>
-				<button className={S.btn}>Conheça a Linx</button>
-				<button className={S.btn}>Ajude o algorítmo</button>
-				<button className={S.btn}>Seus produtos</button>
-				<button className={S.btn}>Compartilhe</button>
+				<article>
+					<p className={S.p1}>uma seleção especial de produtos</p>
+					<h3 className={S.boldText}>especial para você</h3>
+				</article>
+
+				<p className={S.p2}>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+
+				<div className={S.btndiv}>
+					<button className={S.btn}>Conheça a Linx</button>
+					<button className={S.btn}>Ajude o algorítmo</button>
+					<button className={S.btn}>Seus produtos</button>
+					<button className={S.btn}>Compartilhe</button>
+				</div>
 			</div>
-        </div>
-        <div className={S.triangle}></div>
-    </header>
-  )
+			<div className={S.triangle}></div>
+		</header>
+	)
 }
